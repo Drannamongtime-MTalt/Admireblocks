33e843fad9a9f757afba0d94f5bb07daa03c7ffc
Drannamongtime-MTalt <117553041+Drannamongtime-MTalt@users.noreply.github.com>
2023-01-17 20:08:10 +0800

    Update admire16.js
    pls work

--- Javascript/admire16.js
+++ Javascript/admire16.js
@@ -1,34 +1,37 @@
 (function(Scratch) {
-    'use strict';
-  
-    if (!Scratch.extensions.unsandboxed) {
-      throw new Error('Admire16 must be run unsandboxed!');
-    }
-  
- // code goes here //
-    class Admire16 {
-      getInfo () {
-        return {
-          id: 'Admire16',
-          name: 'Admire16',
-          blocks: [
-            {
-              opcode: 'ghcmmnj',
-              blockType: Scratch.BlockType.COMMAND,
-              text: 'Open link [LINK]',
-              arguments: {
-                LINK: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: ' ',
-                }
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('MacaoBlocks must be run unsandboxed!');
+  }
+
+  class MacaoBlocks {
+    getInfo () {
+      return {
+        id: 'macaoblocks',
+        name: 'MacaoBlocks',
+        color1: '#69ca99',
+        color2: '#5ca882',
+        color3: '#69c798',
+        blocks: [
+          {
+            opcode: 'ie1',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Open link [im1]',
+            arguments: {
+              im1: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://www.google.com/'
               }
-            },
-        ]
-        }
+            }
+          },
+
+        ],
+      };
     }
-    ghcmmnj (args) {
-        window.open(args.LINK)
-      }
+    ie1 () {
+      window.open(im1);
     }
-    Scratch.extensions.register(new Admire16());
-  })(Scratch);
+  }
+  Scratch.extensions.register(new MacaoBlocks());
+})(Scratch)
