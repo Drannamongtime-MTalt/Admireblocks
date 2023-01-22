c989558efba304afda134b6c7cc516ac91fe8a20
Drannamongtime-MTalt <117553041+Drannamongtime-MTalt@users.noreply.github.com>
2023-01-22 19:46:05 +0800

    Update admire16.js

--- Javascript/admire16.js
+++ Javascript/admire16.js
@@ -1,37 +1,50 @@
-(function(Scratch) {
-  'use strict';
+const BlockType = require('../../extension-support/block-type');
+const ArgumentType = require('../../extension-support/argument-type');
 
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error('MacaoBlocks must be run unsandboxed!');
-  }
-
-  class MacaoBlocks {
+/**
+ * Class
+ * @constructor
+ */
+class macaoblocks {
+    constructor (runtime) {
+        /**
+         * The runtime instantiating this block package.
+         * @type {runtime}
+         */
+        this.runtime = runtime;
+        store.attachRuntime(runtime);
+    /**
+     * @returns {object} metadata for this extension and its blocks.
+     */
+    }
     getInfo () {
-      return {
-        id: 'macaoblocks',
-        name: 'MacaoBlocks',
-        color1: '#69ca99',
-        color2: '#5ca882',
-        color3: '#69c798',
-        blocks: [
-          {
-            opcode: 'ie1',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Open link [im1]',
-            arguments: {
-              im1: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'https://www.google.com/'
-              }
-            }
-          },
+        return {
+            id: 'macaoblocks',
+            name: 'MacaoBlocks',
+            color1: '#659b80',
+            color2: '#5e8e76',
+            color3: '#6ca98b',
+            isDynamic: true,
+            orderBlocks: this.orderCategoryBlocks,
+            blocks: [
+                {
+                    opcode: 'openlink',
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                      link: {
+                          type: ArgumentType.STRING,
+                          defaultValue: "https://scratch.mit.edu"
+                      }
+                  },
+                    text: 'Open link [link]'
+                },
+            ],
+          }
+        }
+    openlink (args) {
+      //Opens link
+      window.open(args.link)
+    }
+}
 
-        ],
-      };
-    }
-    ie1 () {
-      window.open(im1);
-    }
-  }
-  Scratch.extensions.register(new MacaoBlocks());
-})(Scratch)
+module.exports = macaoblocks;
